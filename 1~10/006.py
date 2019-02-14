8c0044a356ea23b75b0b93a395b80bb8535a3707
low-key-Tony <961663316@qq.com>
2019-02-14 16:12:22 +0800

    006: 斐波那契数列

--- 1~10/006.py
+++ 1~10/006.py
@@ -3,13 +3,34 @@
 # @Date    : 2019/2/14 下午3:42
 # @IDE     : PyCharm
 
+import sys
 
+
+# 方法1
 def fib(n):
     a, b = 0, 1
-    for i in range(n - 1):
+    for i in range(n):
         a, b = b, a + b
-        print(a)
+        print(a, end=" ")
 
 
-# 输出了第10个斐波那契数列
-fib(10)
+# 方法2
+def fibonacci(n):  # 生成器函数 - 斐波那契
+    a, b, counter = 0, 1, 0
+    while True:
+        if counter >= n:
+            return
+        a, b = b, a + b
+        counter += 1
+        yield a  # yield 的作用就是把一个函数变成一个 generator(生成器)
+
+
+if __name__ == '__main__':
+    fib(10)
+    print()
+    f = fibonacci(10)
+    while True:
+        try:
+            print(next(f), end=" ")
+        except StopIteration:
+            sys.exit()
