982e108bba752552eec03ee6e1f305a97d62d9c2
low-key-Tony <961663316@qq.com>
2019-02-15 16:53:45 +0800

    009: 暂停1秒输出

---
+++ 1~10/009.py
@@ -0,0 +1,15 @@
+# @Author  : adair_chan
+# @Email   : adairchan.dream@gmail.com
+# @Date    : 2019/2/15 下午4:47
+# @IDE     : PyCharm
+import time
+
+for i in range(1, 5):
+    print(i)
+    """
+    sleep(seconds)
+
+    Delay execution for a given number of seconds.  The argument may be
+    a floating point number for subsecond precision.
+    """
+    time.sleep(1)
