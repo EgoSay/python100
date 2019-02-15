5a32ebb422865049135dcc8c1710571d6c7e0ed4
low-key-Tony <961663316@qq.com>
2019-02-15 16:42:49 +0800

    008: 打印9*9乘法表

---
+++ 1~10/008.py
@@ -0,0 +1,9 @@
+# @Author  : adair_chan
+# @Email   : adairchan.dream@gmail.com
+# @Date    : 2019/2/15 下午4:31
+# @IDE     : PyCharm
+
+for i in range(1, 10):
+    print()
+    for j in range(1, 10):
+        print(" %d*%d=%d" % (i, j, i * j), end=" ")
