1c78f4746a2ddfd7c9d210e78d033bd90aea8296
low-key-Tony <961663316@qq.com>
2019-02-15 16:30:19 +0800

    007: 将一个列表的数据复制到另一个列表中

---
+++ 1~10/007.py
@@ -0,0 +1,13 @@
+# @Author  : adair_chan
+# @Email   : adairchan.dream@gmail.com
+# @Date    : 2019/2/15 下午4:25
+# @IDE     : PyCharm
+
+list1 = [1, 2, 3]
+
+# 方法1
+list2 = list1[:]
+# 方法2
+list3 = list1.copy()
+print(list2)
+
