5fa502019456c959d90608190386ea33162b4d6e
low-key-Tony <961663316@qq.com>
2019-02-25 20:36:17 +0800

    016: 打印指定格式日期

---
+++ 11～20/016.py
@@ -0,0 +1,7 @@
+# @Author  : adair_chan
+# @Email   : adairchan.dream@gmail.com
+# @Date    : 2019/2/25 下午8:28
+# @IDE     : PyCharm
+import time
+
+print("当前时间为:{}".format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
