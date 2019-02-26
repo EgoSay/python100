b7d8e39771d9b3983d1b9c912eed8658aa907531
low-key-Tony <961663316@qq.com>
2019-02-26 17:57:22 +0800

    018: 叠加计算

---
+++ 11～20/018.py
@@ -0,0 +1,19 @@
+# @Author  : adair_chan
+# @Email   : adairchan.dream@gmail.com
+# @Date    : 2019/2/26 下午4:46
+# @IDE     : PyCharm
+
+
+def add(x, y):
+    x_sum = x
+    flag = x
+    for i in range(y):
+        x = x * 10 + flag
+        x_sum += x
+    return x_sum
+
+
+if __name__ == '__main__':
+    a = int(input('请输入a:'))
+    n = int(input('要叠加的次数:'))
+    print('计算结果为:{}'.format(add(a, n)))
