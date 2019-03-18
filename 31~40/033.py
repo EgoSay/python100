7c7738aac08e05ce813968703a3fa9d376a47f60
low-key-Tony <adairchan.dream@gmail.com>
2019-03-18 20:41:38 +0800

    033: 列表以逗号间隔输出

---
+++ 31~40/033.py
@@ -0,0 +1,9 @@
+# @Author  : adair_chan
+# @Email   : adairchan.dream@gmail.com
+# @Date    : 2019/3/18 下午8:12
+# @IDE     : PyCharm
+
+L = [1, 2, 3, 4, 5]
+s1 = ','.join(str(n) for n in L)
+
+print(s1)
