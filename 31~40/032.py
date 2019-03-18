ca07cc16d742013f10018707ec5bad47b2ca5326
low-key-Tony <adairchan.dream@gmail.com>
2019-03-18 10:10:05 +0800

    032: 反转列表

---
+++ 31~40/032.py
@@ -0,0 +1,12 @@
+# @Author  : adair_chan
+# @Email   : adairchan.dream@gmail.com
+# @Date    : 2019/3/18 上午10:00
+# @IDE     : PyCharm
+
+
+li = ['a', 'b', 'c', 'd']
+# 方法一
+print(li[::-1])
+
+# 方法二
+print(li.reverse())
