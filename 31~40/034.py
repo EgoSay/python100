78b806adecdcf52dcd15659287b2bffce13dc458
low-key-Tony <adairchan.dream@gmail.com>
2019-03-18 20:42:10 +0800

    034: 函数调用

---
+++ 31~40/034.py
@@ -0,0 +1,13 @@
+# @Author  : adair_chan
+# @Email   : adairchan.dream@gmail.com
+# @Date    : 2019/3/18 下午8:17
+# @IDE     : PyCharm
+
+
+def hello_world():
+    print("hello, world!")
+
+
+if __name__ == '__main__':
+    for i in range(3):
+        hello_world()
