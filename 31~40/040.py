82c0a2c7c3b94ec4a0fdf7693afdbc398f15571d
low-key-Tony <adairchan.dream@gmail.com>
2019-03-19 11:38:40 +0800

    040: 将一个数组逆序输出

---
+++ 31~40/040.py
@@ -0,0 +1,15 @@
+# @Author  : adair_chan
+# @Email   : adairchan.dream@gmail.com
+# @Date    : 2019/3/18 下午8:39
+# @IDE     : PyCharm
+
+
+"""
+python 索引介绍
+[-1] :    取最后一个元素
+[:-1] :   除去最后一个元素的取全部
+[::-1] :  从最后一个开始翻转顺序取
+[n::-1] : 从下标为 n 的
+"""
+li = [23, 21, 12, 3, 43, 7, 38]
+print(li[::-1])
