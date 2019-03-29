111f79fc1ab6dd926e7d38ce007cdfb281a5383d
low-key-Tony <adairchan.dream@gmail.com>
2019-03-29 09:48:58 +0800

    050: 生成随机数

---
+++ 41~50/050.py
@@ -0,0 +1,10 @@
+# @Author  : adair_chan
+# @Email   : adairchan.dream@gmail.com
+# @Date    : 2019/3/21 上午10:04
+# @IDE     : PyCharm
+import random
+
+# 随机生成一个整数
+print(random.randint(1, 100))
+
+print(random.randrange(0, 100, 3))
