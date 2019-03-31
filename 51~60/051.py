cb439da9cc75b9b67d62991e67daebedab8614e4
low-key-Tony <961663316@qq.com>
2019-03-31 23:30:43 +0800

    051

---
+++ 51~60/051.py
@@ -0,0 +1,10 @@
+# @Author  : adairchan
+# @Email   : adairchan.dream@gmail.com
+# @Date    : 2019/3/31 23:27
+# @IDE     : PyCharm
+
+a = 0o77
+b = a & 3
+print('a & b = %d' % b)
+b &= 7
+print('a & b = %d' % b)
