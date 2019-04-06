c87f9136fed5bb809b0de0f7320d603bbfd2cc6a
low-key-Tony <961663316@qq.com>
2019-04-06 22:31:41 +0800

    055:按位取反

---
+++ 51~60/055.py
@@ -0,0 +1,9 @@
+# @Author  : adairchan
+# @Email   : adairchan.dream@gmail.com
+# @Date    : 2019/4/6 22:25
+# @IDE     : PyCharm
+
+a = 15
+print("{}的二进制表示为:{}".format(a, bin(a)))
+b = ~a
+print("{0}按位取反后的值为:{1},二进制表示为:{2}".format(a, b, bin(b)))
