433b03705add715e71845e8ec311f35186ce5eb4
adair_chan <adairchan.dream@gmail.com>
2019-04-12 22:09:50 +0800

    055：按位取反

---
+++ 51~60/055.py
@@ -0,0 +1,10 @@
+# @Author  : adair_chan
+# @Email   : adairchan.dream@gmail.com
+# @Date    : 2019/4/12 下午9:39
+# @IDE     : PyCharm
+
+a = 234
+b = ~a
+print('The a\'s 1 complement is %d' % b)
+a = ~a
+print('The a\'s 2 complement is %d' % a)
