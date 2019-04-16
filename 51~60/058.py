0dbace24c96fe80854f64b3f6603b26282041f1a
adair_chan <adairchan.dream@gmail.com>
2019-04-16 19:11:01 +0800

    058:画图，学用rectangle画方形

---
+++ 51~60/058.py
@@ -0,0 +1,22 @@
+# @Author  : adair_chan
+# @Email   : adairchan.dream@gmail.com
+# @Date    : 2019/4/16 下午7:08
+# @IDE     : PyCharm
+from tkinter import *
+
+root = Tk()
+root.title('Canvas')
+canvas = Canvas(root, width=400, height=400, bg='yellow')
+x0 = 263
+y0 = 263
+y1 = 275
+x1 = 275
+for i in range(19):
+    canvas.create_rectangle(x0, y0, x1, y1)
+    x0 -= 5
+    y0 -= 5
+    x1 += 5
+    y1 += 5
+
+canvas.pack()
+root.mainloop()
