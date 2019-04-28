9a29789a465dd4a114f74bae938f31933e067e9f
low-key-Tony <961663316@qq.com>
2019-04-28 19:27:21 +0800

    066

---
+++ 61~70/066.py
@@ -0,0 +1,13 @@
+# @Author  : adairchan
+# @Email   : adairchan.dream@gmail.com
+# @Date    : 2019/4/28 19:16
+# @IDE     : PyCharm
+
+li = list()
+n1 = int(input('n1 = :\n'))
+li.append(n1)
+n2 = int(input('n2 = :\n'))
+li.append(n2)
+n3 = int(input('n3 = :\n'))
+li.append(n3)
+print(sorted(li))
