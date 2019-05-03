1922a3b60a89485ba2286a6701baff389fb0a4d9
low-key-Tony <961663316@qq.com>
2019-05-03 23:12:18 +0800

    070

---
+++ 61~70/070.py
@@ -0,0 +1,7 @@
+# @Author  : adairchan
+# @Email   : adairchan.dream@gmail.com
+# @Date    : 2019/5/3 23:10
+# @IDE     : PyCharm
+s = input('please input a string:\n')
+print('the string has %d characters.' % len(s))
+
