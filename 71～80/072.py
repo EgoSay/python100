b028438f8fe78e9a9fa1e36c60af9e397af01997
adair_chan <adairchan.dream@gmail.com>
2019-05-08 22:15:47 +0800

    072

---
+++ 71～80/072.py
@@ -0,0 +1,12 @@
+# @Author  : adair_chan
+# @Email   : adairchan.dream@gmail.com
+# @Date    : 2019/5/8 下午10:14
+# @IDE     : PyCharm
+
+
+if __name__ == '__main__':
+    ptr = []
+    for i in range(5):
+        num = int(input('please input a number:\n'))
+        ptr.append(num)
+    print(ptr)
