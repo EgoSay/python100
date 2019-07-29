636cfb374c08fdf727a11fe78209a4f8cc084ace
adair_chan <adairchan.dream@gmail.com>
2019-07-29 09:14:45 +0800

    084: 拼接字符串

---
+++ 81～90/084.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+# @Author  : adair_chan
+# @Email   : adairchan.dream@gmail.com
+# @Date    : 2019/7/29 上午9:11
+# @IDE     : PyCharm
+
+delimiter = ','
+str_list = ['Brazil', 'Russia', 'India', 'China']
+print(delimiter.join(str_list))
