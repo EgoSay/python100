a645c908f879e3b362db6748b135310cd666fade
adair_chan <adairchan.dream@gmail.com>
2019-08-06 09:43:07 +0800

    086

---
+++ 81～90/086.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+# @Author  : adair_chan
+# @Email   : adairchan.dream@gmail.com
+# @Date    : 2019/8/6 上午9:39
+# @IDE     : PyCharm
+
+"""it is same as the problem 084"""
