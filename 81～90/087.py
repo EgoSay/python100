fb9d83478d4c262a2b88b76de7349088f8d74752
adair_chan <adairchan.dream@gmail.com>
2019-08-06 09:43:18 +0800

    087

---
+++ 81～90/087.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+# @Author  : adair_chan
+# @Email   : adairchan.dream@gmail.com
+# @Date    : 2019/8/6 上午9:41
+# @IDE     : PyCharm
+
+if __name__ == '__main__':
+    class student:
+        x = 0
+        c = 0
+
+
+    def f(stu):
+        stu.x = 20
+        stu.c = 'c'
+
+
+    a = student()
+    a.x = 3
+    a.c = 'a'
+    f(a)
+    print(a.x, a.c)
