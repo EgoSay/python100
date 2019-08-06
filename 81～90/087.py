2fef70d9d18079a74577265dc7339d12065e4bd4
adair_chan <adairchan.dream@gmail.com>
2019-08-06 09:44:26 +0800

    087

--- 81～90/087.py
+++ 81～90/087.py
@@ -6,17 +6,16 @@
 # @IDE     : PyCharm
 
 if __name__ == '__main__':
-    class student:
+    class Student:
         x = 0
         c = 0
-
 
     def f(stu):
         stu.x = 20
         stu.c = 'c'
 
 
-    a = student()
+    a = Student()
     a.x = 3
     a.c = 'a'
     f(a)
