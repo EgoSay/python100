7a2e91f0ff894e9d32fe5a048fb7b20c8da5b341
adair_chan <adairchan.dream@gmail.com>
2019-10-04 22:13:07 +0800

    095

---
+++ 91～100/095.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+# @Author  : adair_chan
+# @Email   : adairchan.dream@gmail.com
+# @IDE     : PyCharm
+
+from dateutil import parser
+dt = parser.parse("Aug 28 2015 12:00AM")
+print(dt)
