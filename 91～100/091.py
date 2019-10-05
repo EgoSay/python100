13adb02c2159c82de5a15baeb81adf64078ab5af
adair_chan <adairchan.dream@gmail.com>
2019-10-05 19:04:26 +0800

    091

---
+++ 91～100/091.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+# @Author  : adair_chan
+# @Email   : adairchan.dream@gmail.com
+# @IDE     : PyCharm
+
+if __name__ == '__main__':
+    import time
+
+    print(time.ctime(time.time()))
+    print(time.asctime(time.localtime(time.time())))
