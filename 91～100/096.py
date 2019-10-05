73e395298014c11d856d54d5d7710e039489d9de
adair_chan <adairchan.dream@gmail.com>
2019-10-05 19:04:35 +0800

    096

---
+++ 91～100/096.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+# @Author  : adair_chan
+# @Email   : adairchan.dream@gmail.com
+# @IDE     : PyCharm
+
+
+if __name__ == "__main__":
+    str1 = input("请输入一个字符串:")
+    str2 = input("请输入一个子字符串: ")
+    print(str1.count(str2))
